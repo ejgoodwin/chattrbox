113eee9a523f05985e9134765049d8976a11c175
ejgoodwin <ericagoodwin90@gmail.com>
2018-03-03 12:35:38 +0000

    Silver challenge p322 - password addition

--- websockets-server.js
+++ websockets-server.js
@@ -2,6 +2,12 @@
 var WebSocket = require('ws');
 var WebSocketServer = WebSocket.Server;
 var port = 3001;
+var Prompt = require('prompt-password');
+var prompt = new Prompt({
+	type: 'password',
+	message: 'Enter your password',
+	name: 'password'
+});
 // Server is established and bound to port
 var ws = new WebSocketServer({
 	port: port
@@ -10,14 +16,27 @@ console.log('websockets server started');
 // Array to store messages
 var messages = [];
 
+// prompt.run()
+//   .then(function(answers) {
+//     console.log(answers)
+//   });
+
 // Callback for any connection events for WebSockets Server
 // When client makes connection to server, that connection is accessable through 'socket' object - function's argument
 ws.on('connection', function(socket) {
 	console.log('client connection established');
-	// Allow new users to see previous messages
-	messages.forEach(function(msg) {
-		socket.send(msg);
-	});
+	// Prompt new users for a password. If user enters correct password, show them previous messages
+	prompt.run()
+	  .then(function(answers) {
+	    console.log(answers)
+	    if(answers == 'swordfish') {
+	    	// Allow new users to see previous messages
+	    	messages.forEach(function(msg) {
+	    		socket.send(msg);
+	    	});
+	    }
+	  });
+	
 
 	// Repeat messages sent to server - called an 'echo server'
 	socket.on('message', function(data) {
@@ -25,6 +44,7 @@ ws.on('connection', function(socket) {
 		// Add messages to array
 		messages.push(data);
 		ws.clients.forEach(function(clientSocket) {
+			// Repeat messages - increment number of repetitions by one each time a message is added
 			let i = 0;
 			while( i < messages.length) {
 				clientSocket.send(data);
