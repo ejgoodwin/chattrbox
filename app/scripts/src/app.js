1f919f5527a626b7a1593304e8c03aeaa6d5ecc8
ejgoodwin <ericagoodwin90@gmail.com>
2018-03-31 21:21:45 +0100

    chapter 18 adding UI functionality

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -1,14 +1,37 @@
 import socket from './ws-client';
+import {UserStore} from './storage';
+// curly braces indicate a 'named import' - declares a local variable named ChatForm and binds it to value from dom.js
+import {ChatForm, ChatList, promptForUsername} from './dom';
+
+const FORM_SELECTOR = '[data-chat="chat-form"]';
+const INPUT_SELECTOR = '[data-chat="message-input"]';
+const LIST_SELECTOR = '[data-chat="message-list"]';
+
+let userStore = new UserStore('x-chatterbox/u');
+let username = userStore.get();
+if(!username) {
+  username = promptForUsername();
+  userStore.set(username);
+}
+
 
 class ChatApp {
 	constructor() {
+		this.chatForm = new ChatForm(FORM_SELECTOR, INPUT_SELECTOR);
+		this.chatList = new ChatList(LIST_SELECTOR, username);
+
 		socket.init('ws://localhost:3001');
 		socket.registerOpenHandler(() => {
-			let message = new ChatMessage({ message: 'Pow!'});
-			socket.sendMessage(message.serialize());
+			this.chatForm.init((data) => {
+				let message = new ChatMessage({message: data});
+				socket.sendMessage(message.serialize());
+			});
+      this.chatList.init();
 		});
 		socket.registerMessageHandler((data) => {
 			console.log(data);
+			let message = new ChatMessage(data);
+			this.chatList.drawMessage(message.serialize());
 		});
 	}
 }
@@ -16,7 +39,7 @@ class ChatApp {
 class ChatMessage {
 	constructor({
 		message: m,
-		user: u='batman',
+		user: u = username,
 		timestamp: t=(new Date()).getTime()
 	}) {
 		this.message = m;
@@ -31,7 +54,5 @@ class ChatMessage {
 		};
 	}
 }
-
-
 
 export default ChatApp;
